8a002d6cc53148dc4a1ae2aef9bde85caf61ea2a
ovidiubizo <ovidiu.bizo@golfgenius.com>
2022-06-20 11:15:47 +0300

    fix extra line in routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   namespace :api do
     post 'login', to: 'users#login'
     get 'feed', to: 'scores#user_feed'
-    get 'users', to: 'users#index'
     resources :scores, only: %i[create destroy show]
     resources :users, only: %i[index show]
   end
